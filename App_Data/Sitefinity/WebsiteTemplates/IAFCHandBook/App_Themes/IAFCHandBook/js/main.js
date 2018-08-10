a40f3ccccd82ba017624c44f362f0289a4588182
ossusa <maxt@oss-usa.com>
2018-08-10 19:28:51 +0300

    s

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -1,21 +1,21 @@
-/* Adding the script tag to the head as suggested before */
-
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "https://code.jquery.com/jquery-2.2.1.min.js";
-
-// Then bind the event to the callback function.
-// There are several events for cross browser compatibility.
-script.onreadystatechange = handler;
-script.onload = handler;
-
-// Fire the loading
-head.appendChild(script);
-
-function handler(){
-    console.log('jquery added :)');
-}
+// /* Adding the script tag to the head as suggested before */
+//
+// var head = document.getElementsByTagName('head')[0];
+// var script = document.createElement('script');
+// script.type = 'text/javascript';
+// script.src = "https://code.jquery.com/jquery-2.2.1.min.js";
+//
+// // Then bind the event to the callback function.
+// // There are several events for cross browser compatibility.
+// script.onreadystatechange = handler;
+// script.onload = handler;
+//
+// // Fire the loading
+// head.appendChild(script);
+//
+// function handler(){
+//     console.log('jquery added :)');
+// }
 
 $( document ).ready(function() {
     $('.hb-jumbo__search').appendTo(".hb-jumbo");
