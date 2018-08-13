93db8b24cd709c5687a7734a3ca91d54a364392f
ossusa <maxt@oss-usa.com>
2018-08-13 14:56:23 +0300

    js test

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -1,113 +1,108 @@
+$( document ).ready(function() {
+/*Jumbo Search*/
+    $('.hb-jumbo__search').appendTo(".hb-jumbo");
+    $(".k-input").attr("placeholder", "Search");
 
-
-    $(document).ready(function() {
-        /*Jumbo Search*/
-        $('.hb-jumbo__search').appendTo(".hb-jumbo");
-        $(".k-input").attr("placeholder", "Search");
-
-        /*Slider*/
-        $('.resources__slider').slick({
-            dots: false,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            responsive: [
-                {
-                    breakpoint: 1109,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
+/*Slider*/
+    $('.resources__slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1109,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
-            ]
-        });
-
-
-        /*Search in header*/
-        var $search = '<li class="k-item k-item-search"><button class="header__search anticon anticon-search" type="submit"></button></li>';
-        var $li = $( ".sfNavHorizontalDropDownWrp li:contains('Account')" );
-        var $liSearch = '<div class="mg-search-box hidden"><div class="relative"><label for="site-search" class="visuallyhidden">Search</label><input type="text" class="search-box" id="site-search" placeholder="Search"><button id="site-search-submit" class="hidden__search anticon anticon-search" type="submit"></button></div></div>';
-        $($search).insertAfter($li);
-        $( $liSearch ).appendTo('.k-item-search');
-        // $(document).on('click', '.header__search', event => {
-        //     event.preventDefault();
-        //     $('.mg-search-box').toggleClass('hidden');
-        // });
-        // $(document).on('click', '#site-search-submit', event => {
-        //     event.preventDefault();
-        //     var q = $('#site-search').val();
-        //     location.href = '/search-results/#/' + q + "/page=1";
-        // });
-
-        /*Off canvas menu*/
-        $(".header__mob-open").click(function(){
-            $('.header__mob-nav').css({ width: "100vw" });
-        });
-        $(".header__mob-close").click(function(){
-            $('.header__mob-nav').css({ width: "0" });
-        });
-
-
-        /*Accordion*/
-        $(".accordion__title").on("click", function(e) {
-
-            e.preventDefault();
-            var $this = $(this);
-
-            if (!$this.hasClass("accordion-active")) {
-                $(".accordion__content").slideUp(400);
-                $(".accordion__title").removeClass("accordion-active");
-                $('.accordion__arrow').removeClass('accordion__rotate');
             }
+]
+});
 
-            $this.toggleClass("accordion-active");
-            $this.next().slideToggle();
-            $('.accordion__arrow',this).toggleClass('accordion__rotate');
-        });
+/*Search in header*/
+    var $search = '<li class="k-item k-item-search"><button class="header__search anticon anticon-search" type="submit"></button></li>';
+    var $li = $( ".sfNavHorizontalDropDownWrp li:contains('Account')" );
+    var $liSearch = '<div class="mg-search-box hidden"><div class="relative"><label for="site-search" class="visuallyhidden">Search</label><input type="text" class="search-box" id="site-search" placeholder="Search"><button id="site-search-submit" class="hidden__search anticon anticon-search" type="submit"></button></div></div>';
+    $($search).insertAfter($li);
+    $( $liSearch ).appendTo('.k-item-search');
+    $(document).on('click', '.header__search', event => {
+    	event.preventDefault();
+    	$('.mg-search-box').toggleClass('hidden');
+    });
+$(document).on('click', '#site-search-submit', event => {
+        event.preventDefault();
+        var q = $('#site-search').val();
+        location.href = '/search-results/#/' + q + "/page=1";
+   });
 
-        $(".accordion__sub-title").on("click", function(e) {
+/*Off canvas menu*/
+$(".header__mob-open").click(function(){
+    $('.header__mob-nav').css({ width: "100vw" });
+});
+$(".header__mob-close").click(function(){
+    $('.header__mob-nav').css({ width: "0" });
+});
 
-            e.preventDefault();
-            var $this = $(this);
 
-            if (!$this.hasClass("accordion-sub-active")) {
-                $(".accordion__sub-content").slideUp(400);
-                $(".accordion__sub-title").removeClass("accordion-sub-active");
-                $('.accordion__sub-arrow').removeClass('accordion__rotate');
-            }
+    /*Accordion*/
+    $(".accordion__title").on("click", function(e) {
 
-            $this.toggleClass("accordion-sub-active");
-            $this.next().slideToggle();
-            $('.accordion__sub-arrow',this).toggleClass('accordion__rotate');
-        });
+        e.preventDefault();
+        var $this = $(this);
 
-        /*Arrow*/
-        $arrowRight = '<div class="anticon anticon-right"></div>';
-        $(".header__nav > ul > li > div > ul > li > a").append( $arrowRight );
-        /*Resources Widget*/
-        $('.resources').prepend( $( ".resources__logged" ) );
-        //
-        // $('.community__head-btn').on('click', event => {
-        //     $(event.currentTarget).addClass('community__head-btn-active');
-        // });
-        /*Select*/
-        // $('.community__title-select').selectric({
-        //     nativeOnMobile: false
-        // });
-        $('.selectric-community__title-select').find('.button').addClass('title__arrow');
-        $('.selectric-community__title-select .selectric').click(function(){
-            $('.title__arrow').toggleClass('rotated');
-        });
+        if (!$this.hasClass("accordion-active")) {
+            $(".accordion__content").slideUp(400);
+            $(".accordion__title").removeClass("accordion-active");
+            $('.accordion__arrow').removeClass('accordion__rotate');
+        }
 
-        // $('.community__category-select').selectric({
-        //     nativeOnMobile: false
-        // });
-        $('.selectric-community__category-select').find('.button').addClass('category__arrow');
-        $('.selectric-community__category-select .selectric').click(function(){
-            $('.category__arrow').toggleClass('rotated');
-        });
-console.log('Test')
-
+        $this.toggleClass("accordion-active");
+        $this.next().slideToggle();
+        $('.accordion__arrow',this).toggleClass('accordion__rotate');
     });
 
+    $(".accordion__sub-title").on("click", function(e) {
+
+        e.preventDefault();
+        var $this = $(this);
+
+        if (!$this.hasClass("accordion-sub-active")) {
+            $(".accordion__sub-content").slideUp(400);
+            $(".accordion__sub-title").removeClass("accordion-sub-active");
+            $('.accordion__sub-arrow').removeClass('accordion__rotate');
+        }
+
+        $this.toggleClass("accordion-sub-active");
+        $this.next().slideToggle();
+        $('.accordion__sub-arrow',this).toggleClass('accordion__rotate');
+    });
+
+    /*Arrow*/
+    $arrowRight = '<div class="anticon anticon-right"></div>';
+    $(".header__nav > ul > li > div > ul > li > a").append( $arrowRight );
+/*Resources Widget*/
+    $('.resources').prepend( $( ".resources__logged" ) );
+    $('.community__head-btn').on('click', event => {
+        $(event.currentTarget).addClass('community__head-btn-active');
+    });
+    /*Select*/
+    $('.community__title-select').selectric({
+        nativeOnMobile: false
+    });
+    $('.selectric-community__title-select').find('.button').addClass('title__arrow');
+    $('.selectric-community__title-select .selectric').click(function(){
+        $('.title__arrow').toggleClass('rotated');
+    });
+
+    $('.community__category-select').selectric({
+        nativeOnMobile: false
+    });
+    $('.selectric-community__category-select').find('.button').addClass('category__arrow');
+    $('.selectric-community__category-select .selectric').click(function(){
+        $('.category__arrow').toggleClass('rotated');
+    });
+
+
+});
