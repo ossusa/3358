5f9dddd54b3da2faf206943383639e0a1fac23c9
ossusa <maxt@oss-usa.com>
2018-08-17 19:22:12 +0300

    js

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -86,19 +86,22 @@ $(document).ready(function () {
     $('.community__head-btn').click(function () {
         $(this).addClass('community__head-btn-active');
     });
-    global.jQuery = require('jquery');
-    var selectric = require('selectric')(jQuery);
+
     /*Select*/
-    jQuery('.community__title-select').selectric({
+    $(function () {
+    $('.community__title-select').selectric({
         nativeOnMobile: false
+    });
     });
     $('.selectric-community__title-select').find('.button').addClass('title__arrow');
     $('.selectric-community__title-select .selectric').click(function (e) {
         e.stopPropagation();
         $('.title__arrow').toggleClass('rotated');
     });
-    jQuery('.community__category-select').selectric({
+    $(function () {
+    $('.community__category-select').selectric({
         nativeOnMobile: false
+    });
     });
     $('.selectric-community__category-select').find('.button').addClass('category__arrow');
     $('.selectric-community__category-select .selectric').click(function (e) {
