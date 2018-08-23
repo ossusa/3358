93c67d99b7d2add77c6107f37d23a4165ddfce0d
Vasyl Voloshyn <voloshyn@edsson.com>
2018-08-23 10:33:14 +0300

    Added Cancel Button. Menu preparation

---
+++ Mvc/Models/IAFCHandBookMyHandBookMenuItemModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+	public class IAFCHandBookMyHandBookMenuItemModel
+	{
+		public IAFCHandBookMyHandBookMenuItemModel()
+		{
+			ChildMenuItem = new List<IAFCHandBookMyHandBookMenuItemModel>();
+			Visible = true;
+			ChildExists = false;
+		}
+
+		public String Url { get; set; }
+		public String Title { get; set; }
+		public Boolean Visible { get; set; }
+		public Boolean ChildExists { get; set; }
+		public List<IAFCHandBookMyHandBookMenuItemModel> ChildMenuItem { get; set; }
+	}
+}
---
+++ Mvc/Models/IAFCHandBookMyHandBookMenuModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+	public class IAFCHandBookMyHandBookMenuModel
+	{
+		public IAFCHandBookMyHandBookMenuModel()
+		{
+			Menu = new List<IAFCHandBookMyHandBookMenuItemModel>();
+		}
+		public List<IAFCHandBookMyHandBookMenuItemModel> Menu { get; set; }
+	}
+}
