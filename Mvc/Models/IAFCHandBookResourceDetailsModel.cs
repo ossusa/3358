5926e29d5f302d9a285b0bcf32b282c22b278eb7
Vasyl Voloshyn <voloshyn@edsson.com>
2018-08-31 15:52:32 +0300

    Fixed Resource Details Link

--- Mvc/Models/IAFCHandBookResourceDetailsModel.cs
+++ Mvc/Models/IAFCHandBookResourceDetailsModel.cs
@@ -7,13 +7,15 @@ namespace SitefinityWebApp.Mvc.Models
 {
 	public class IAFCHandBookResourceDetailsModel
 	{
+		public IAFCHandBookResourceDetailsModel()
+		{
+			Duration = new TimeSpan();
+			Category = new IAFCHandBookTopicCategoryModel();
+		}
 		public Guid id { get; set; }
 		public string ResourceTitle { get; set; }
-
 		public string ResourceSummary { get; set; }
-
 		public string ResourceDescription { get; set; }
-
 		public IAFCHandBookTopicCategoryModel Category { get; set; }
 		public string ResourceType { get; set; }
 		public TimeSpan Duration { get; set; }
@@ -21,11 +23,7 @@ namespace SitefinityWebApp.Mvc.Models
 		public string ImageUrl { get; set; }
 		public string ImageSvgUrl { get; set; }
 		public string ImagePlaceholderUrl { get; set; }
-		public string VideoEmbedCode { get; set; }
-		public IAFCHandBookResourceDetailsModel()
-		{
-			Duration = new TimeSpan();
-			Category = new IAFCHandBookTopicCategoryModel();
-		}
+		public string VideoEmbedCode { get; set; }		
+		public bool IsResourceHasMoreThen1Category { get; set; }
 	}
 }
