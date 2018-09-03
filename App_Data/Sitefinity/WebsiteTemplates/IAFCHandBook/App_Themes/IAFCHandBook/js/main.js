140de402d90652042be88517931b504f44d5be73
ossusa <maxt@oss-usa.com>
2018-09-03 17:51:08 +0300

    css

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -171,7 +171,7 @@ $(document).ready(function() {
     $(".header__list:nth-of-type(1) > a").click(function(){
         $(".header__list:nth-of-type(1) > a").addClass("colored-list");
         if(! $(".header__sub_ul").is(':visible') ) {
-            $(".header__list:nth-of-type(1) > a").removeClass("colored");
+            $(".header__list:nth-of-type(1) > a").removeClass("colored-list");
         }
     });
 });
