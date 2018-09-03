7e7618e075b0cf550991b7e4d4065123dbb9e8ec
ossusa <maxt@oss-usa.com>
2018-09-03 14:51:59 +0300

    js for search added

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
     $("#site-search-submit").click(function (e) {
         e.preventDefault();
         var q = $('#site-search').val();
-        location.href = '/search-results/#/' + q + "/page=1";
+        location.href = 'iafchandbookhome/search-results/' + q;
     });
 
     /*Off canvas menu*/
