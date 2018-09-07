1056c1df1e1d712b262a7f07d21c36eecddede26
ossusa <support@oss-usa.com>
2018-09-07 17:11:35 +0300

    css

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -349,3 +349,9 @@ function myFunction(x) {
 var x = window.matchMedia("(min-width: 767px) and (max-width: 1109px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
+
+$('.header_unlogged .header__sub_list-link').each(function() {
+    $(this).on("click", function(){
+        $('.header_unlogged .header__second_ul').toggle();
+    });
+});
