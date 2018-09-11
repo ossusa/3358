89c37eb3e71f31e4fe54519c11ebc3e8b257846f
OSS <witchertheavenger@gmail.com>
2018-09-11 20:31:45 +0300

    text truncate

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -191,33 +191,35 @@ $(document).ready(function () {
 
     setInterval(function() {
         setImgHeight();
+
+        //text truncate
+        $('.resources__slide-desc').each(function(){
+            $(this).ellipsis({
+                lines: 2,             // force ellipsis after a certain number of lines. Default is 'auto'
+                ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
+                responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
+            });
+        });
+
+        $('.resources__slide-title').each(function(){
+            $(this).ellipsis({
+                lines: 1,             // force ellipsis after a certain number of lines. Default is 'auto'
+                ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
+                responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
+            });
+        });
+
+        $('.resources__slide-article').each(function(){
+            $(this).ellipsis({
+                lines: 1,             // force ellipsis after a certain number of lines. Default is 'auto'
+                ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
+                responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
+            });
+        });
     }, 250);
 
 
-    //text truncate
-    $('.resources__slide-desc').each(function(){
-        $(this).ellipsis({
-            lines: 2,             // force ellipsis after a certain number of lines. Default is 'auto'
-            ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
-            responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
-        });
-    });
 
-    $('.resources__slide-title').each(function(){
-        $(this).ellipsis({
-            lines: 1,             // force ellipsis after a certain number of lines. Default is 'auto'
-            ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
-            responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
-        });
-    });
-
-    $('.resources__slide-article').each(function(){
-        $(this).ellipsis({
-            lines: 1,             // force ellipsis after a certain number of lines. Default is 'auto'
-            ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
-            responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
-        });
-    });
 
 
 
