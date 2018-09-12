9212cd776b1ecb27398ed68f6c3fa2da71c3a1ab
OSS <witchertheavenger@gmail.com>
2018-09-12 15:48:48 +0300

    text truncate

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -195,28 +195,9 @@ $(document).ready(function () {
     }, 250);
 
 
-    $('.foo').ellipsis({
-        lines: 3,             // force ellipsis after a certain number of lines. Default is 'auto'
-        ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
-        responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
-    });
 
 
-    //text truncate
-    $('.resources__slide-desc').each(function(){
-        $(this).ellipsis({
-            lines: 2,             // force ellipsis after a certain number of lines. Default is 'auto'
-            responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
-        });
-    });
 
-    $('.resources__slide-title').each(function(){
-        console.log(this);
-        $(this).ellipsis({
-          lines: 2,           // force ellipsis after a certain number of lines. Default is 'auto'
-          responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
-      });
-    });
 
 
 
@@ -425,6 +406,22 @@ $(window).on('load', function() {
                 'height': 'auto'
             });
         }
+    });
+
+    //text truncate
+    $('.resources__slide-desc').each(function(){
+        $(this).ellipsis({
+            lines: 2,             // force ellipsis after a certain number of lines. Default is 'auto'
+            responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
+        });
+    });
+
+    $('.resources__slide-title').each(function(){
+        console.log(this);
+        $(this).ellipsis({
+            lines: 2,           // force ellipsis after a certain number of lines. Default is 'auto'
+            responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
+        });
     });
 
 });
