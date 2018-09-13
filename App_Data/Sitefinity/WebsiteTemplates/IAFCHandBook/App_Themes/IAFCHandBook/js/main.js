298a56113a7306d5089d8c59876e2b840e5e73e4
Nikita Fitisov <fitisovn@gmail.com>
2018-09-13 19:29:09 +0300

    js comment

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -295,21 +295,21 @@ $(document).ready(function() {
         $(this).toggleClass('clicked-dislike');
         $('.addCommentLike').removeClass('clicked-like');
     });
-    
+
     /*COOKIES LIKES*/
-    $('.addLike').addClass('cookie-like').attr('data-type', 'like');
-    $('.addDislike').addClass('cookie-dislike').attr('data-type', 'dislike');
-    function refreshButtonState(buttonElement) {
-        var buttonType = $(buttonElement).attr('data-type');
-        $(buttonElement).toggleClass('clicked', $.cookie(buttonType));
-    }
-
-    $('.cookie-like, .cookie-dislike').on('click', function (e) {
-        var buttonType = $(this).attr('data-type');
-        $.cookie(buttonType, !($(this).is('.clicked')) ? 1 : 0);
-
-        refreshButtonState(this);
-    });
+    // $('.addLike').addClass('cookie-like').attr('data-type', 'like');
+    // $('.addDislike').addClass('cookie-dislike').attr('data-type', 'dislike');
+    // function refreshButtonState(buttonElement) {
+    //     var buttonType = $(buttonElement).attr('data-type');
+    //     $(buttonElement).toggleClass('clicked', $.cookie(buttonType));
+    // }
+    //
+    // $('.cookie-like, .cookie-dislike').on('click', function (e) {
+    //     var buttonType = $(this).attr('data-type');
+    //     $.cookie(buttonType, !($(this).is('.clicked')) ? 1 : 0);
+    //
+    //     refreshButtonState(this);
+    // });
 
 
 
