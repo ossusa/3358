9f2e84e33cfbaca38f9cebc39ff5f0e753a1347b
ossusa <support@oss-usa.com>
2018-09-20 12:41:09 +0300

    js

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -447,9 +447,9 @@ if($(window).width() > 1109) {
             }
         });
     }
-    if(window.matchMedia('(max-width: 990px) and (min-width: 901px)').matches) {
-        $('.comment_span').text($('.comment_span').text().replace('Comments', ''));
-    }
+    // if(window.matchMedia('(max-width: 990px) and (min-width: 901px)').matches) {
+    //     $('.comment_span').text($('.comment_span').text().replace('Comments', ''));
+    // }
 });
 
 
