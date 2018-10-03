7bd994831bf5dd0f04aa7d77e1aa60a4836d5402
ossusa <support@oss-usa.com>
2018-10-03 13:02:17 +0300

    js

--- App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
+++ App_Data/Sitefinity/WebsiteTemplates/IAFCHandBook/App_Themes/IAFCHandBook/js/main.js
@@ -21,13 +21,13 @@ $(document).ready(function () {
     if (/MSIE 10/i.test(navigator.userAgent)) {
         window.alert('isIE10');
         $('.logo').hide();
-        $('.header__logo > .sfimageWrp').append('$ieLogo');
+        $('.header__logo > .sfimageWrp').append($ieLogo);
     }
 
     if (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
         window.location = 'pages/core/ie.htm';
         $('.logo').hide();
-        $('.header__logo > .sfimageWrp').append('$ieLogo');
+        $('.header__logo > .sfimageWrp').append($ieLogo);
     }
 
     if (/Edge\/\d./i.test(navigator.userAgent)){
